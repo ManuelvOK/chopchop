898fe4ba44b9c21767e9442d78e651be2fd66844
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-06-17 18:24:40 +0200

    Print generation result

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
+#include <eval.h>
 #include <input.h>
 
 int main(int argc, char *argv[]) {
     printf("UNSATISFIABLE\n");
     FILE* input = fopen("input", "r");
     struct jsp jsp = eval_input(input);
+    puts("");
+    print_schedule(generate_schedule(&jsp));
     fclose(input);
     return 0;
 }
