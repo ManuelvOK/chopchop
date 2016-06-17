48fd82da01d614913d6db95e5efe50d4883e568f
manuelthieme <manuelthieme@googlemail.com>
2016-06-17 18:18:57 +0200

    finished input evaluation

--- src/input.c
+++ src/input.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
-#include <array/array.h>
+#include <array.h>
 #include <input.h>
 
 struct jsp eval_input(FILE* input_file) {
     /* extract first line from file */
-    char *line;
-    fscanf(input_file, "%ms", &line);
+    char line[256] = {0};
+    fgets(line, 255, input_file);
+    printf("%s\n", line);
 
     /* scan job- and operation count from first line */
     int job_count;
     int machine_count;
     sscanf(line, "%d %d", &job_count, &machine_count);
-    free(line);
-
     /* init job array */
     array(struct job, jobs, job_count);
 
@@ -21,16 +20,22 @@ struct jsp eval_input(FILE* input_file) {
 
     /* iterate through file lines */
     for (int job = 0; job < job_count; ++job) {
-        fscanf(input_file, "%ms", &line);
+        fgets(line, 255, input_file);
         jobs[job] = init_job(job, 0);
 
         /* iterate through operations */
-        while (2 == sscanf(line, "%d, %d ", machine, duration)) {
-            struct operation op = {machine, duration};
-            apush(jobs[job].ops, op);
+        char *nptr = line;
+        char *endptr = NULL;
+        for (;;) {
+            machine = strtol(nptr, &endptr, 0);
+            nptr = endptr;
+            duration = strtol(nptr, &endptr, 0);
+            if (nptr == endptr)
+                break;
+            printf("(%d | %d) ", machine, duration);
         }
-
-        /* free memory since it is new allocated in next iteration */
-        free(line);
+        printf("\n");
     }
+    struct jsp jsp = {machine_count, jobs};
+    return jsp;
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <input.h>
 
 int main(int argc, char *argv[]) {
     printf("UNSATISFIABLE\n");
+    FILE* input = fopen("input", "r");
+    struct jsp jsp = eval_input(input);
+    fclose(input);
     return 0;
 }
