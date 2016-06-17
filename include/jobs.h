235e9ed8dfed8926da4c9146c99e9c14393151c5
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-06-17 15:57:44 +0200

    Datatypes and init functions

--- include/jobs.h
+++ include/jobs.h
@@ -1,3 +1,30 @@
 #ifndef JOBS_H
 #define JOBS_H value
+
+/* operation:
+ * a job takes <duration> time on <machine>
+ */
+struct operation {
+    unsigned machine;
+    unsigned duration;
+};
+
+/* job:
+ * a job named <id> with operations <ops>
+ */
+struct job {
+    unsigned id;
+    struct operation *ops; /* dynamic array */
+};
+
+/* init_job:
+ * allocate and initialise a job named <id> <n_nops> operations
+ */
+struct job *init_job(unsigned id, unsigned n_ops);
+
+/* init_operation:
+ * allocate and initialise an operation on <machine> taking <duration> time
+ */
+struct operation *init_operation(unsigned machine, unsigned duration);
+
 #endif /* ifndef JOBS_H */
--- src/jobs.c
+++ src/jobs.c
@@ -1,1 +1,23 @@
 #include <jobs.h>
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include <array/array.h>
+
+struct job *init_job(unsigned id, unsigned n_ops) {
+    struct job *j = malloc(sizeof *j);
+    assert(j != NULL);
+    j->id = id;
+    array(struct operation, ops, n_ops);
+    j->ops = ops;
+    return j;
+}
+
+struct operation *init_operation(unsigned machine, unsigned duration) {
+    struct operation *op = malloc(sizeof *op);
+    assert(op != NULL);
+    op->machine = machine;
+    op->duration = duration;
+    return op;
+}
