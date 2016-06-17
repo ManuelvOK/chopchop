a48a855abc64bbc9441fd6cee753a018abcf3ff0
manuelthieme <manuelthieme@googlemail.com>
2016-06-17 16:36:59 +0200

    added file input evaluation

--- include/input.h
+++ include/input.h
@@ -1,3 +1,12 @@
 #ifndef INPUT_H
 #define INPUT_H value
+
+#include <stdio.h>
+#include <jobs.h>
+
+/**
+ * Reads the input file into a jsp struct.
+ */
+struct jsp eval_input(FILE* input_file);
+
 #endif /* ifndef INPUT_H */
--- src/input.c
+++ src/input.c
@@ -1,1 +1,36 @@
+#include <stdlib.h>
+#include <array/array.h>
 #include <input.h>
+
+struct jsp eval_input(FILE* input_file) {
+    /* extract first line from file */
+    char *line;
+    fscanf(input_file, "%ms", &line);
+
+    /* scan job- and operation count from first line */
+    int job_count;
+    int machine_count;
+    sscanf(line, "%d %d", &job_count, &machine_count);
+    free(line);
+
+    /* init job array */
+    array(struct job, jobs, job_count);
+
+    int machine;
+    int duration;
+
+    /* iterate through file lines */
+    for (int job = 0; job < job_count; ++job) {
+        fscanf(input_file, "%ms", &line);
+        jobs[job] = init_job(job, 0);
+
+        /* iterate through operations */
+        while (2 == sscanf(line, "%d, %d ", machine, duration)) {
+            struct operation op = {machine, duration};
+            apush(jobs[job].ops, op);
+        }
+
+        /* free memory since it is new allocated in next iteration */
+        free(line);
+    }
+}
