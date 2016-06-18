e10d4b9140f2f33b4c7f020a49831409a41b416b
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-06-18 11:46:12 +0200

    Changed data structures for storing jobs and operations

--- include/jobs.h
+++ include/jobs.h
@@ -2,38 +2,32 @@
 #define JOBS_H value
 
 /* operation:
- * a job takes <duration> time on <machine>
+ * the <nth> operation of a job named <job_id> takes <duration> time on
+ * <machine>
  */
 struct operation {
+    unsigned job;
+    unsigned nth;
     unsigned machine;
     unsigned duration;
 };
 
-/* job:
- * a job named <id> with operations <ops>
+/* job shop scheduling problem:
+ * <operations> must be scheduled on <n_machines> machines in total
  */
-struct job {
-    unsigned id;
-    struct operation *ops; /* dynamic array */
-};
-
 struct jsp {
     unsigned n_machines;
-    struct job *jobs; /* dynamic array */
+    struct operation *operations; /* dynamic array */
 };
 
-/* init_job:
- * initialise a job named <id> <n_nops> operations
- */
-struct job init_job(unsigned id, unsigned n_ops);
-
 /* init_operation:
- * initialise an operation on <machine> taking <duration> time
+ * initialise the <nth> operation of <job> on <machine> taking <duration> time
  */
-struct operation init_operation(unsigned machine, unsigned duration);
+struct operation init_operation(unsigned job, unsigned nth,
+                                unsigned machine, unsigned duration);
 
 /* init_jsp_data:
- * initialise the problem specification with an empty job list and 0 machines
+ * initialise the jsp specification with an empty operation list and 0 machines
  */
 struct jsp init_jsp_data(void);
 
--- src/jobs.c
+++ src/jobs.c
@@ -5,23 +5,17 @@
 
 #include <array.h>
 
-struct job init_job(unsigned id, unsigned n_ops) {
-    struct job j;
-    j.id = id;
-    array(struct operation, ops, n_ops);
-    j.ops = ops;
-    return j;
-}
-
-struct operation init_operation(unsigned machine, unsigned duration) {
-    struct operation op = {.machine = machine, .duration = duration};
+struct operation init_operation(unsigned job, unsigned nth,
+                                unsigned machine, unsigned duration) {
+    struct operation op = {.job = job, .nth = nth, .machine = machine,
+                           .duration = duration};
     return op;
 }
 
 struct jsp init_jsp_data(void) {
     struct jsp js;
     js.n_machines = 0;
-    array(struct job, j, 0);
-    js.jobs = j;
+    array(struct operation, ops, 0);
+    js.operations = ops;
     return js;
 }
