04ae3f31a7281719dc023676346e1c41ec89de50
manuelthieme <manuelthieme@googlemail.com>
2016-06-18 14:48:11 +0200

    added get_neighbour implementation

--- src/hood.c
+++ src/hood.c
@@ -1,1 +1,32 @@
 #include <hood.h>
+#include <stdlib.h>
+
+#include <array.h>
+
+struct jsp get_neighbour(const struct jsp schedule) {
+    /* get a random index to swap */
+    unsigned index;
+    do {
+        index = (rand() / (RAND_MAX * 1.0))
+                    * (alength(schedule.operations) - 1);
+    } while (schedule.operations[index].job
+             == schedule.operations[index + 1].job);
+
+    /* create new jsp */
+    struct jsp new_jsp = init_jsp_data(alength(schedule.operations));
+    new_jsp.n_jobs = schedule.n_jobs;
+    new_jsp.n_machines = schedule.n_machines;
+
+    /* copy operation data and swap two of them */
+    aforeach (i, schedule.operations) {
+        if (i == index) {
+            new_jsp.operations[i] = schedule.operations[i + 1];
+        } else if (i == index + 1) {
+            new_jsp.operations[i] = schedule.operations[i - 1];
+        } else {
+            new_jsp.operations[i] = schedule.operations[i];
+        }
+    }
+
+    return new_jsp;
+}
--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include <eval.h>
 #include <input.h>
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
     printf("UNSATISFIABLE\n");
     FILE* input = fopen("input", "r");
     struct jsp jsp = eval_input(input);
