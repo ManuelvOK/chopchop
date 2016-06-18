a60b917b97814813f4e9f76298d5db92b864147d
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-06-18 12:25:18 +0200

    Neighbourhood skeleton

--- include/hood.h
+++ include/hood.h
@@ -1,3 +1,12 @@
 #ifndef HOOD_H
 #define HOOD_H value
+
+#include <jobs.h>
+
+/* get_neighbour:
+ * get a randomly selected schedule from the neighbour of the given <schedule>
+ * note: the contents of the given schedule aren't changed
+ */
+struct jsp get_neighbour(const struct jsp schedule);
+
 #endif /* ifndef HOOD_H */
